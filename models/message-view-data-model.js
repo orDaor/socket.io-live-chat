f092782345dc7ebe103bb7cd2b3b05ba7aef7583
Orgher Dalipi <orgher.dalipi@gmail.com>
2022-10-27 12:56:18 +0200

    Added sendingFailed property in MessageViewData class

--- models/message-view-data-model.js
+++ models/message-view-data-model.js
@@ -3,12 +3,17 @@ class MessageViewData {
   //a Message class object can be passed to the constructor, and
   //the user id of the user requesting this message to be displayed.
   //If no message is passed, then single message data should be passed
-  constructor(message, viewerId) {
+  constructor(message, viewerId, sendingFailed) {
     //general message data
     this.text = message.text;
     this.creationDate = message.creationDate;
     this.senderIsViewer = message.senderId === viewerId;
     this.messageId = message.messageId;
+    if (sendingFailed) {
+      this.sendingFailed = sendingFailed;
+    } else {
+      this.sendingFailed = false;
+    }
   }
 }
 
