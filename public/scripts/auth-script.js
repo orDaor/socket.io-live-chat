bd161bdc79b227f182047673f8aa2a3faf83c6c2
Orgher Dalipi <orgher.dalipi@gmail.com>
2022-11-08 23:02:04 +0100

    Reset flags at logout

--- public/scripts/auth-script.js
+++ public/scripts/auth-script.js
@@ -24,6 +24,7 @@ function logout(event) {
   selectedChatItemGlobal = undefined;
   selectedMessageItemGlobal = undefined;
   lastGeneratedInvitationLinkGlobal = "";
+  lastInvitationIdAcceptedGlobal = "";
   disableDisplayOfScrollToBottomButton = false;
   disableLoadingOfMoreMessages = false;
 
